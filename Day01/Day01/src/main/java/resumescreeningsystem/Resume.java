a7eb1566397f03a9b8a7a31010b9d7cbc3186285
Karan Kumar <officialkaran39@gmail.com>
2025-02-11 22:59:44 +0530

    [Karan Kumar]Add. Added the program for ai driven resume screening system

---
+++ Day01/Day01/src/main/java/resumescreeningsystem/Resume.java
@@ -0,0 +1,13 @@
+package resumescreeningsystem;
+
+class Resume<T extends JobRole> {
+    private T jobRole;
+
+    public Resume(T jobRole) {
+        this.jobRole = jobRole;
+    }
+
+    public String processResume() {
+        return "Processing Resume for: " + jobRole.getJobDescription();
+    }
+}
