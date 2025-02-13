e297bc7b3e565ae040bed01f7ceb69cc642a8c71
Karan Kumar <officialkaran39@gmail.com>
2025-02-13 11:37:01 +0530

    [Karan Kumar]Add. Added the program for finding that one set is a subset of another

---
+++ Day02/Day02/src/test/java/setinterface/findsubsets/FindSubsetsTest.java
@@ -0,0 +1,38 @@
+package setinterface.findsubsets;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindSubsetsTest {
+    Set<Integer> set1;
+    Set<Integer> set2;
+
+    @BeforeEach
+    void setUp(){
+        set1 = new HashSet<>(Arrays.asList(2,3));
+        set2 = new HashSet<>(Arrays.asList(1,2,3,4));
+
+
+    }
+
+
+    @Test
+    void checkSubset() {
+        set1.add(100);
+        set2.add(100);
+        assertTrue(FindSubsets.checkSubset(set1,set2),"Function error");
+        System.out.println("Test 1 Passed !!!");
+    }
+    @Test
+    void checkSubset2(){
+        set1.add(333);
+        assertFalse(FindSubsets.checkSubset(set1,set2), "Function Error");
+        System.out.println("Test 2 Passed !!!");
+    }
+}
