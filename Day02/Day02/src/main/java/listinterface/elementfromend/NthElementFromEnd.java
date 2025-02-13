a07b2b8b4d576c4cb75c2c49ad40c9238e353cc5
Karan Kumar <officialkaran39@gmail.com>
2025-02-13 10:59:56 +0530

    [Karan Kumar]Add. Added the program for finding the nth element from the end of linked list

---
+++ Day02/Day02/src/main/java/listinterface/elementfromend/NthElementFromEnd.java
@@ -0,0 +1,29 @@
+package listinterface.elementfromend;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+public class NthElementFromEnd {
+    public static void main(String[] args) {
+        LinkedList<Character> list = new LinkedList<>(Arrays.asList('A','B','C','D','E'));
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ListIterator<Character> first = list.listIterator();
+        ListIterator<Character> second = list.listIterator();
+        for(int i = 0;i<n;i++){
+            if(!first.hasNext()){
+                System.out.println("Invalid Index");
+                return;
+            }
+            first.next();
+        }
+        while(first.hasNext()){
+            first.next();
+            second.next();
+        }
+        System.out.println(n+"th element from the end: "+second.next());
+
+    }
+}
