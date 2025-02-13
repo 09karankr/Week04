9013caadc0318aaec2c44f538a5785e8f3ad27eb
Karan Kumar <officialkaran39@gmail.com>
2025-02-13 13:27:30 +0530

    [Karan Kumar]Add. Added the program for design a voting management system using hashmap

---
+++ Day02/Day02/src/main/java/mapinterface/votingsystem/Main.java
@@ -0,0 +1,19 @@
+package mapinterface.votingsystem;
+
+
+public class Main {
+    public static void main(String[] args) {
+        VotingSystem votingSystem = new VotingSystem();
+
+        votingSystem.castVote("Alice");
+        votingSystem.castVote("Bob");
+        votingSystem.castVote("Alice");
+        votingSystem.castVote("Charlie");
+        votingSystem.castVote("Bob");
+        votingSystem.castVote("Alice");
+
+        votingSystem.displayResultsAlphabetically();
+        votingSystem.displayResultsByVotes();
+        votingSystem.displayVoteOrder();
+    }
+}
