e4ec3387cf85154b0af7ac3b931c9bfaf8e99c33
Karan Kumar <officialkaran39@gmail.com>
2025-02-13 10:29:29 +0530

    [Karan Kumar]Add. Added reverse list program using collection

---
+++ Day02/Day02/src/test/java/listinterface/reverselist/ReverseListTest.java
@@ -0,0 +1,43 @@
+package listinterface.reverselist;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseListTest {
+    LinkedList<Integer> linkedList;
+    ArrayList<Integer> arrayList;
+    @BeforeEach
+    void setUp(){
+        linkedList = new LinkedList<>();
+        arrayList = new ArrayList<>();
+        linkedList.add(5);
+        linkedList.add(6);
+        linkedList.add(7);
+        linkedList.add(8);
+        arrayList.add(12);
+        arrayList.add(13);
+        arrayList.add(15);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void reverseLinkedListTest() {
+        int firstNumber = linkedList.getFirst();
+        ReverseList.reverseLinkedList(linkedList);
+        assertEquals(firstNumber,linkedList.getLast(),"Error in reversal function");
+    }
+
+    @org.junit.jupiter.api.Test
+    void reverseArrayListTest() {
+        int firstNumber = arrayList.get(0);
+        ReverseList.reverseArrayList(arrayList);
+        assertEquals(firstNumber,arrayList.get(arrayList.size() - 1),"Error in reversal function");
+    }
+}
