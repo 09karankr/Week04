19512d199480fc65eec23753388b4dabeca66c87
Karan Kumar <officialkaran39@gmail.com>
2025-02-13 11:20:47 +0530

    [Karan Kumar]Add. Added the program for union and intersection of sets

---
+++ Day02/Day02/src/main/java/setinterface/unionandintersectionofsets/UnionAndIntersection.java
@@ -0,0 +1,30 @@
+package setinterface.unionandintersectionofsets;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UnionAndIntersection {
+    public static Set<Integer> getUnion(Set<Integer> set1, Set<Integer> set2){
+        Set<Integer> ans = new HashSet<>(set1);
+        ans.addAll(set2);
+        return ans;
+
+
+    }
+    public static Set<Integer> getIntersection(Set<Integer> set1, Set<Integer> set2){
+        Set<Integer> ans = new HashSet<>(set1);
+        ans.retainAll(set2);
+        return ans;
+    }
+    public static void main(String[] args) {
+
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(11,12,13));
+        Set<Integer> set2 = new HashSet<>(Arrays.asList(13,14,15));
+        System.out.println("Set 1: "+set1);
+        System.out.println("Set 2: "+set2);
+        System.out.println("Union: "+getUnion(set1,set2));
+        System.out.println("Intersection: "+getIntersection(set1,set2));
+
+    }
+}
