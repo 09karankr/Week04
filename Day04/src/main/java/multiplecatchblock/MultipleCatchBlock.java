c6933cd2507d8dcb44e7de6797dea7a46603d8bd
Karan Kumar <officialkaran39@gmail.com>
2025-02-15 13:14:05 +0530

    [Karan Kumar]Add. Added the program for performsarray operations, and also handled the ArrayIndexOutOfBoundsException and NullPointerException

---
+++ Day04/src/main/java/multiplecatchblock/MultipleCatchBlock.java
@@ -0,0 +1,58 @@
+package multiplecatchblock;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MultipleCatchBlock {
+    public static void indexValue(int[] arr, int index){
+        if(arr == null){
+            throw new NullPointerException("Array is not initialized!!");
+        }
+        if (index < 0){
+            throw new ArrayIndexOutOfBoundsException("index out of bounds!");
+        }
+        System.out.println("Value at index" + index + " : " + arr[index]);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] array = null;
+        System.out.println("Enter index value size " + "for array");
+        try{
+            int size = sc.nextInt();
+            array = new int[size];
+
+        } catch (InputMismatchException e){
+            System.out.println("Invalid input for array");
+            sc.next();
+            return;
+        }
+        if(array != null) {
+            System.out.println("Enter value for array");
+            for (int i = 0; i < array.length; i++) {
+                while (true) {
+                    System.out.println("Enter value for " + (i + 1) + " : ");
+                    try {
+                        array[i] = sc.nextInt();
+                        break;
+                    } catch (InputMismatchException e) {
+                        System.out.println("Invalid input ! try Again.");
+                        sc.next();
+                    }
+                }
+            }
+
+        }
+        System.out.println("enter the index to search");
+        try{
+            int index = sc.nextInt();
+            indexValue(array,index);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("array index out of bound" + e.getMessage());
+        }catch (NullPointerException e){
+            System.out.println("array is not intialised  "  + e.getMessage());
+        }catch (InputMismatchException e){
+            System.out.println("input for array is not correct!!"+ e.getMessage());
+        }
+
+    }
+}
