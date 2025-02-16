2ab2eb136f01ef5b4ae567efe514ecd659a324ee
Karan Kumar <officialkaran39@gmail.com>
2025-02-16 21:24:29 +0530

    [Karan Kumar]Add. Added the program for validating credit card numbers

---
+++ Day05/src/main/java/advanceproblems/validatecreditcard/ValidateCreditCard.java
@@ -0,0 +1,22 @@
+package advanceproblems.validatecreditcard;
+
+public class ValidateCreditCard {
+    public static boolean isValidVisa(String cardNumber){
+        return cardNumber.matches("^4\\d{15}$");
+    }
+    public static boolean isValidMasterCard(String cardNumber){
+        return cardNumber.matches("^5\\d{15}$");
+    }
+
+    public static void main(String[] args) {
+
+        String  VisaNumber = "4123456789012345";
+        String masterCard = "5123456789012345";
+        String invalidCard = "6123456789012345";
+
+        System.out.println("Visa card details : " + isValidVisa(VisaNumber));
+        System.out.println("Is valid master Card : " + isValidMasterCard(masterCard));
+        System.out.println("is invaild card details :" + isValidVisa(invalidCard));
+
+    }
+}
