79a879729031f8fd6f1e4b1ce73d529a9e933ae6
Karan Kumar <officialkaran39@gmail.com>
2025-02-16 21:00:36 +0530

    [Karan Kumar]Add. Added the program for replacing multiple space with one space

---
+++ Day05/src/main/java/replaceandmodify/ReplaceMultipleSpace.java
@@ -0,0 +1,13 @@
+package replaceandmodify;
+
+public class ReplaceMultipleSpace {
+    public static void main(String[] args) {
+        String input = "This   is an   example with   multiple   spaces.";
+
+        String output = input.replaceAll("\\s+"," ");
+
+        System.out.println("input with multiple space : " + input);
+        System.out.println("output with single space : " + output);
+
+    }
+}
