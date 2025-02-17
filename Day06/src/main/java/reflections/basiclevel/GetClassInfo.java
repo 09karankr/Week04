f19af7fd39d739d6bcebb73ec4b18a23821536fe
Karan Kumar <officialkaran39@gmail.com>
2025-02-17 23:02:49 +0530

    [Karan Kumar]Add. Added the program for getting the class info using reflection

--- Day06/src/main/java/reflections/basiclevel/GetClassInfo.java
+++ Day06/src/main/java/reflections/basiclevel/GetClassInfo.java
@@ -3,6 +3,7 @@ package reflections.basiclevel;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GetClassInfo {
