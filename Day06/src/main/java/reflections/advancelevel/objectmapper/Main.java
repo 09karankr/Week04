c3fbd5819c648479e4b39638d65641cfc96d3f29
Karan Kumar <officialkaran39@gmail.com>
2025-02-17 23:45:20 +0530

    [Karan Kumar]Add. Added the program for custom object mapper and creating a method toObject that uses reflection to set field values from a given map

---
+++ Day06/src/main/java/reflections/advancelevel/objectmapper/Main.java
@@ -0,0 +1,20 @@
+package reflections.advancelevel.objectmapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Map<String, Object> properties =  new HashMap<>();
+        properties.put("name","Karan Kumar");
+        properties.put("age", 22);
+        try{
+            Person person = ObjectMapper.toObject(Person.class,properties);
+            System.out.println(person);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ Day06/src/main/java/reflections/advancelevel/objectmapper/Person.java
@@ -0,0 +1,14 @@
+package reflections.advancelevel.objectmapper;
+
+public class Person {
+    private String name;
+    private int age;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
