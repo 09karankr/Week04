b043f860e30b9907a8aa97d7164c3d3ae9a4ee49
Karan Kumar <officialkaran39@gmail.com>
2025-02-17 22:18:05 +0530

    [Karan Kumar]Add. Added the program for access private field of a class and modify and retrieve its value

---
+++ Day06/src/main/java/reflections/basiclevel/AccessPrivateFIelds.java
@@ -0,0 +1,29 @@
+package reflections.basiclevel;
+
+import java.lang.reflect.Field;
+
+class Person{
+    private int age = 18;
+}
+public class AccessPrivateFIelds {
+    public static void main(String[] args) throws Exception {
+        Person person = new Person();
+        Class<?> cls = person.getClass();
+
+        //access private
+        Field field = cls.getDeclaredField("age");
+        field.setAccessible(true);
+
+        //get field value
+
+        System.out.println("original age is " + field.get(person));
+
+        //modify values
+
+        field.set(person, 22);
+
+        //get modified value
+
+        System.out.println("Modified age is " + field.get(person));
+    }
+}
