e073a09ada67b6bb3a2381f2c34d0d50358e5d2e
Karan Kumar <officialkaran39@gmail.com>
2025-02-17 23:06:00 +0530

    [Karan Kumar]Add. Added the program for invoking private methods using reflection and display the result

---
+++ Day06/src/main/java/reflections/basiclevel/InvokePrivateMethod.java
@@ -0,0 +1,24 @@
+package reflections.basiclevel;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+
+class Calculator {
+    private int multiply(int a, int b){
+        return a*b;
+    }
+}
+public class InvokePrivateMethod {
+    public static void main(String[] args) throws Exception {
+        Calculator calculator = new Calculator();
+        Class<?> cls = calculator.getClass();
+
+        //access private method
+        Method method = cls.getDeclaredMethod("multiply", int.class, int.class);
+        method.setAccessible(true);
+
+        //invoke dynamically
+        int result  = (int) method.invoke(calculator,5,10);
+        System.out.println("multiply result is : " + result);
+    }
+}
